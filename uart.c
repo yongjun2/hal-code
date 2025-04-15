33146aafc95a90be7ce94e112c9376f1298fc929
yongjun2 <33952192+yongjun2@users.noreply.github.com>
2025-04-15 22:26:20 +0900

    Update uart.c

--- uart.c
+++ uart.c
@@ -7,10 +7,9 @@ void UART1_ISR (void)
 	UINT8	NextNum;
 
 	INT_ID = (xU1_IIR >> 1) & 0x07;
-	
 	if(INT_ID == 0x02)		// Received Data Available
 	{
-		for(i=0 ; i<8 ; i++)
+		for(i=0 ; i<8 ; i++) // hw buffer size:8
 		{
 			NextNum = (r1_in+1) & (UART1_RXBUF_SIZE-1);
 			if(NextNum != r1_out)
@@ -59,5 +58,61 @@ void UART1_ISR (void)
 
 	INT_ID = (xU1_IIR >> 1) & 0x07;
 #endif
+}
+void ZSYS_UART0_PUT(UINT8 c)
+{
+	UINT8	_ES0;
+	UINT8	_RFIE;
+	UINT8	Len;
+	
+	while(1)
+	{
+		Len = (UART0_TXBUF_SIZE-1);
+		Len = Len - ZSYS_UART0_TXLEN();
+		if(Len > 3)	break;	
+	}
+
+	_RFIE = RFIE;
+	RFIE = 0;
+	_ES0 = ES0;
+	ES0 = 0;
+
+	if(t0_fifo_empty)
+	{
+		t0_fifo_empty = 0;
+		xU0_THR = c;
+	}
+	else
+	{
+		t0buf[t0_in] = c;
+		t0_in++; 
+		t0_in = t0_in & (UART0_TXBUF_SIZE-1);
+	}
+
+	ES0 = _ES0;
+	RFIE = _RFIE;	
+	
+}
+
+
+UINT8 ZSYS_UART0_GET(UINT8 *pc)
+{
+	UINT8	_ES0;
+	UINT8	_RFIE;
+
+	if(ZSYS_UART0_RXLEN() == 0)	return 0;
+
+	_RFIE = RFIE;
+	RFIE = 0;
+	_ES0 = ES0;
+	ES0 = 0;
+	
+	*pc = r0buf[r0_out++];	
+	r0_out = r0_out & (UART0_RXBUF_SIZE-1);
+
+	ES0 = _ES0;
+	RFIE = _RFIE;
+	
+	return 1;
 }
 
